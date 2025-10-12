1b8c00c4324a49ab52a7335c133d2f14131a1ad3
agordavidt <agordavid14@gmail.com>
2025-10-12 08:03:08 +0100

    updated admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -97,7 +97,7 @@
     </div>
 </div>
 
-<div class="row">
+<!-- <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
@@ -110,7 +110,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
 
 @push('scripts')
